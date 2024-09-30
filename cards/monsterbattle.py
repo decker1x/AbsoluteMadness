f14da505d7e00ad7ae5d4e1758d2ed316f7797b4
Daniel Sehn <164395922+sehnterprise@users.noreply.github.com>
2024-09-29 23:07:01 -0400

    Create monsterbattle.py

---
+++ cards/monsterbattle.py
@@ -0,0 +1,8 @@
+import random
+# Game Settings
+run = 1
+while run == 1:
+    # Open a booster pack
+    # Ask to play again
+    print("Type 1 to play again. Type 0 to quit")
+    run = int(input())
