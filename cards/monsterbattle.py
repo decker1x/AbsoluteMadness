ed1160b86450c7316f9d1ae1edbe5b16c4b3cf18
Daniel Sehn <selfsehntered@gmail.com>
2024-09-29 23:45:53 -0400

    Update monsterbattle.py

--- cards/monsterbattle.py
+++ cards/monsterbattle.py
@@ -1,6 +1,20 @@
 import random
+# Monster Card Base
+class monstercard:
+    def __init__(self,name, attack, defense):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+    def attack(self,attack,defense):
+        return
+# Player 1
+
 # Game Settings
 run = 1
+HeavyAllowed = 2
+MidAllowed = 6
+LightAllowed = 12
+SpellsAllowed = 5
 while run == 1:
     # Open a booster pack
     # Ask to play again
